b5a7239d5c1e06c7d501d6904512e5f61e241e19
Asmaa-1999 <asmaa0abdelkhalek@gmail.com>
2024-08-27 16:18:56 +0300

    ..

--- src/components/input.test.js
+++ src/components/input.test.js
@@ -1,7 +1,7 @@
 
 import React from 'react';
 import { render, screen, fireEvent } from '@testing-library/react';
-import Input from './Input';
+import Input from './input';
 
 test('should render Input component with label', () => {
   render(<Input label="Name" value="" onChange={() => {}} />);
